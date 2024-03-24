ea60be09410b15f74accc2c0e762d7a11c12606f
Chaeyoung714 <202102760@hufs.ac.kr>
2024-03-24 13:21:33 +0900

    [feat] Member/models도 1차 작성

--- member/models/member_models.py
+++ member/models/member_models.py
@@ -1,4 +1,16 @@
 from django.db import models
+from board.models.tech_models import Tech
+from board.models.job_models import Job
 
 class Member(models.Model):
     name = models.CharField()
+
+
+
+class MemberTech(models.Model):
+    member = models.ForeignKey(Member, on_delete=models.CASCADE, related_name='post_tech')
+    tech = models.ForeignKey(Tech, on_delete=models.CASCADE, verbose_name='기술스택')
+
+class MemberJob(models.Model):
+    member = models.ForeignKey(Member, on_delte=models.CASCADE, related_name='post_job')
+    job = models.ForeignKey(Job, on_delete=models.CASCADE, verbose_name='직업군스택')
